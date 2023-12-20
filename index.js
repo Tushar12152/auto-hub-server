16c113f8b68b8cd6a4ac8fbd490aae9db09504ce
Tushar12152 <tusharimran470@gmail.com>
2023-12-20 19:43:56 +0600

    users data receive from client side and send to database

--- index.js
+++ index.js
@@ -30,13 +30,35 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const userCollection = client.db("AutoHubDB").collection("Users");
+    
+
+
+    app.post('/users',async(req,res)=>{
+        const user=req.body;
+         
+        const result =await userCollection.insertOne(user)
+        res.send(result)
+        // console.log(user);
+
+    })
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
