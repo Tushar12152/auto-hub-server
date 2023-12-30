d99968c863f7bbb2168f918f240881a5923154fa
Tushar12152 <tusharimran470@gmail.com>
2023-12-30 11:54:16 +0600

    single car is get from db by id

--- index.js
+++ index.js
@@ -72,6 +72,14 @@ async function run() {
          res.send(result)
 
     })
+   
+     app.get('/cars/:id',async (req,res)=>{
+          const id=req.params.id;
+          const query={_id:new ObjectId(id)}
+          const result=await carCollection.findOne(query)
+          res.send(result)
+     })
+
 
     app.delete('/cars/:id',async(req,res)=>{
          const id=req.params.id;
